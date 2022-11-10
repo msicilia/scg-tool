8424c9bfcf13881eeeef114e1fbfb3fff75d25e5
Miguel-Angel Sicilia <msicilia@gmail.com>
2022-11-10 16:02:07 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
+import { grommet, Grommet, Header, Image, Box, Footer } from 'grommet';
+import {Github, Linkedin} from 'grommet-icons';
+import Link from 'next/link';
+import Head from 'next/head';
+import { ModelContextProvider } from './ModelContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <ModelContextProvider>
+          <Grommet theme={grommet}>
+              <Head>
+                 <title>SME Cluster Growth Self assessment</title>
+              </Head>
+                <Header background="light-1" margin="small">
+                  <Box width="xsmall" xheight="small" >
+                  <Link href="/">
+                    <Image  src="/cropped-sme-growth-cluster-FINAL-LOGO.png"></Image>
+                  </Link>
+                  </Box>
+                </Header>
+            <Component {...pageProps} />
+            <Box align="center" background="light-1">
+              <Footer margin="small"> 
+                 <a href="http://github.com">         
+                     <Github size="medium"></Github>
+               </a>
+               <a href="http://linkedin.com">         
+                     <Linkedin size="medium"></Linkedin>
+               </a>
+      </Footer> 
+    </Box>
+    </Grommet>
+    </ModelContextProvider> 
 }
 
 export default MyApp
